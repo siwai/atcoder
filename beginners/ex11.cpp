4bd15d8b76dfafac776546d470324608f60dedc5
siwai <shoka.iwai@gmail.com>
2019-04-07 23:59:34 +0900

    Add

---
+++ beginners/ex11.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, A;
+  cin >> N >> A;
+  int sum = A;
+
+
+
+  // ここにプログラムを追記
+for (int i = 0; i < N; i++) {
+      int x;
+      string op;
+      cin >> op >> x;
+
+   if (op == "+") {
+      A += x;
+    }
+    else if (op == "-") {
+      A -= x;
+    }
+    else if (op == "*") {
+      A *= x;
+    }
+    else if (op == "/" && x != 0) {
+      A /= x;
+    }
+
+    else {
+      cout << "error" << endl;
+      break;
+    }
+  cout << i + 1 << ":" << sum << endl;
+}
+
+
+}
