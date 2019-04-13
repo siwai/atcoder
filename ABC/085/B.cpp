809c7b2767bf9ff07e52d30737ebbcf478293470
siwai <shoka.iwai@gmail.com>
2019-04-13 19:02:45 +0900

    Add 085

---
+++ ABC/085/B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> A.at(i);
+    }
+
+
+    vector<int> num(110);
+
+    for (int j = 0; j < N; ++j) {
+        num.at(A.at(j))++;
+    }
+
+    int res =0;
+    for (int i = 1; i < 101; ++i) {
+        // cout << num.at(i);
+        if(num.at(i)){
+            res++;
+        }
+    }
+    cout << res << endl;
+
+}
