de425615ba88a9ddbfa93191280cfb7b6618e601
siwai <shoka.iwai@gmail.com>
2019-04-19 17:44:15 +0900

    Add

--- ABC/123/b.cpp
+++ ABC/123/b.cpp
@@ -5,9 +5,37 @@ using namespace std;
 int main() {
 
     int A, B, C, D, E;
+    vector<int> dish;
 
-    cin >> A >> B >> C >> D >> E;
+    //cin >> A >> B >> C >> D >> E;
+    for (int i = 0; i < 5; ++i) {
+        cin >> dish.at(i);
+    }
 
+    int a,m;
+    int min  = -1;
+    int count =0;
+    for (int i = 0; i < 5; ++i) {
+        a = dish.at(i) % 2;
+        if (a == 0){
+            m = dish.at(i) / 10;
+            //m +=1;
+        }
+        if (a != 0){
+            if (min == -1){
+                min = a;
+            }
+            else if (min >= a){
+                min = a;
+            }
+            m = dish.at(i) / 10 ;
+            m +=1;
+        }
+        count += m;
+        // dish(i) = m;
+    }
+    int ans;
+    ans = count * 10 + min;
 
-    cout << k << endl;
+    cout << ans << endl;
 }
