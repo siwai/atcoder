f78b148f54953527f2b1fee19b7fcbbfec82c63a
siwai <shoka.iwai@gmail.com>
2019-05-03 18:57:01 +0900

    Add 084 D

--- ABC/122/c.cpp
+++ ABC/122/c.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main() {
 
-    int N, Q; cin >> N >> Q;
+    int N, Q;
+    cin >> N >> Q;
     string S;
     cin >> S;
 
@@ -12,42 +13,26 @@ int main() {
 
     // ACか
     vector<int> a(N, 0);
-    for (int j = 0; j < N-1; ++j) {
-        if (S.at(j) == 'A' && S.at(j+1) == 'C'){
-            a[j] == 1;
+    for (int j = 0; j < N - 1; ++j) {
+        if (S.at(j) == 'A' && S.at(j + 1) == 'C') {
+            a[j] = 1;
         }
     }
 
 
     // 累積和
-    vector<int> s(N+1, 0);
+    vector<int> s(N + 1, 0);
 
     for (int k = 0; k < N; ++k) {
-        s[k+1] = s[k] + a[k];
+        s[k + 1] = s[k] + a[k];
     }
-//
-//    for (int m = 0; m < N; ++m) {
-//        cout << m << ":" << s[m] << endl;
-//    }
-
-
-    string aaa = "hoge";
-    if (aaa == "hoge\"){
-        cout << aaa;
-    }
-    for (int m = 0; m < N; ++m) {
-
-
-        cout << m << ":" << a[m] << endl;
-    }
-
     // クエリ処理
     for (int i = 0; i < Q; ++i) {
-        int l,r; cin >> l >> r;
-        int ans = s[l] - s[r+1];
+        int l, r;
+        cin >> l >> r;
+        int ans = s[r - 1] - s[l - 1];
         cout << ans << endl;
     }
-
 
 
 }
