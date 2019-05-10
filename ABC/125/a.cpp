f07addcc3e2cc7fcd34426cf34d15e788ea89080
siwai <shoka.iwai@gmail.com>
2019-05-11 01:30:56 +0900

    Add ABC 125 a

---
+++ ABC/125/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int A, B, T;
+    cin >> A >> B >> T;
+    int currentTime = A;
+    int ans = 0;
+    while (currentTime <= T) {
+        ans += B;
+        currentTime += A;
+    }
+    cout << ans << endl;
+}
