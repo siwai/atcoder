b2975140d89ba9d16410427fcd005c71846bc55e
siwai <shoka.iwai@gmail.com>
2019-05-12 03:51:43 +0900

    Add ABC 006 A

---
+++ ABC/006/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    string s;
+    if (N % 3 == 0)s = "YES";
+    if (N % 3 != 0)s = "NO";
+
+    cout << s << endl;
+
+}
