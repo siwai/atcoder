bfa0f2d810d6e769b6528ab79a89d41e8d4f169d
siwai <shoka.iwai@gmail.com>
2019-05-13 18:21:20 +0900

    Add ABC 006 B

---
+++ ABC/006/b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> a(N, 0);
+    a[2] = 1;
+    for (int i = 3; i < N; ++i) {
+        a[i] = a[i - 1] + a[i - 2] + a[i - 3];
+        if (a[i] >= 10007) {
+            a[i] = a[i] % 10007;
+        }
+    }
+    int ans = a[N - 1];
+    cout << ans << endl;
+
+}
