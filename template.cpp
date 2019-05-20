430130563a7e46204288b086bbb13692d7fafcb4
siwai <shoka.iwai@gmail.com>
2019-05-20 16:31:52 +0900

    Add GCD and output to template.cpp

--- template.cpp
+++ template.cpp
@@ -7,8 +7,42 @@
 #define vl vector<ll>
 #define vvi vector< vector<int> >
 #define vvl vector< vector<ll> >
+#define repd(i, a, b) for (int i=(a);i<(b);i++)
+#define rep(i, n) repd(i,0,n)
+#define ALL(v) v.begin(), v.end()
+#define INF 1e9
 
 using namespace std;
+
+/**
+ * calculate GCD(greatest common divisor)
+ * @param a
+ * @param b
+ * @return
+ */
+unsigned euclidean_gcd(unsigned a, unsigned b) {
+    if (a < b) return euclidean_gcd(b, a);
+    unsigned r;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+/**
+ * output answer
+ * @tparam T
+ * @param answer
+ * @return
+ */
+template<typename T>
+T output(T answer) {
+    cout << answer << endl;
+    return 0;
+}
+
+/** ------------------------------------------------------------------------ */
 
 int main() {
 
