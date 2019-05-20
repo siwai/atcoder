64b567657a3850864ad43b7c280eca4b80de4398
siwai <shoka.iwai@gmail.com>
2019-05-20 15:22:36 +0900

    Fix template.cpp

--- template.cpp
+++ template.cpp
@@ -3,6 +3,10 @@
 #define ld long double
 #define ll long long int
 #define ull unsigned long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector< vector<int> >
+#define vvl vector< vector<ll> >
 
 using namespace std;
 
