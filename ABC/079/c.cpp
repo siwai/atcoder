e839b7a78b98e5e9d2b5397f57f2867f1bf40175
siwai <shoka.iwai@gmail.com>
2019-05-25 00:46:18 +0900

    Add ABC 079 C

--- ABC/079/c.cpp
+++ ABC/079/c.cpp
@@ -1,38 +1,45 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
+int main(){
+    // 入力
+    string ABCD;
+    cin >> ABCD;
 
-    int N; cin >> N;
-    int A = 7;
+    // A, B, C, Dを個別の変数に定義
+    int A, B, C, D;
+    A = ABCD[0] - '0';
+    B = ABCD[1] - '0';
+    C = ABCD[2] - '0';
+    D = ABCD[3] - '0';
 
-    int x;
-    vector<int> a(4);
+    // A, B, C, Dをまとめたベクターを宣言
+    vector<int> num = {A, B, C, D};
+    // 出力用に'+', '-'の文字を要素とするベクターを宣言
+    vector<char> op = {'+', '-'};
 
-    a[0] = N / 1000;
-    x = (N % 1000);
-    a[1] = x /100;
-    x = x % 100;
-    a[2] = x / 10;
-    x = x % 10;
-    a[3] = x / 1;
-
-
-    for (int j = 0; j < 3; ++j) {
-
-        
+    // ビット全探索　ビットパターン 2^3 = 8
+    for (int i = 0; i < (1 << 3); i++) {
+        int sum = A;
+        // ビットパターンと'+', '-'を関連付け
+        // "0":'+'
+        // "1":'-'
+        for (int j = 0; j < 3; j++) {
+            if (i >> j & 1) sum -= num[j + 1];
+            else            sum += num[j + 1];
+        }
+        // 出力
+        if (sum == 7) {
+            cout << A;
+            for (int k = 0; k < 3; k++) {
+                // ビットパターンiからベクターopの文字を選択
+                cout << op[(i >> k) & 1] << num[k + 1];
+                if (k == 2) {
+                    cout << "=7" << endl;
+                    return 0;
+                }
+            }
+        }
     }
-    
-    
-    
-    for (int i = 0; i < 4; ++i) {
-        cout << a.at(i);
-    }
-
-
-
-
-
-
 }
