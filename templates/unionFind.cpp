77d670aad507ca7361107318f25db3b95227205e
siwai <shoka.iwai@gmail.com>
2019-05-24 15:19:38 +0900

    Add Template UnionFind

---
+++ templates/unionFind.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+#define ld long double
+#define ll long long int
+#define ull unsigned long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector< vector<int> >
+#define vvl vector< vector<ll> >
+#define repd(i, a, b) for (int i=(a);i<(b);i++)
+#define rep(i, n) repd(i,0,n)
+#define ALL(v) v.begin(), v.end()
+#define INF 1e9
+
+using namespace std;
+
+/**
+ * calculate GCD(greatest common divisor)
+ * @param a
+ * @param b
+ * @return
+ */
+unsigned euclidean_gcd(unsigned a, unsigned b) {
+    if (a < b) return euclidean_gcd(b, a);
+    unsigned r;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+/**
+ * change minimum
+ * @tparam T
+ * @param a
+ * @param b
+ * @return bool
+ */
+template<class T>
+inline bool changeMinimum(T &a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * change maximum
+ * @tparam T
+ * @param a
+ * @param b
+ * @return bool
+ */
+template<class T>
+inline bool changeMaximum(T &a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * output answer
+ * @tparam T
+ * @param answer
+ * @return
+ */
+template<typename T>
+T output(T answer) {
+    cout << answer << endl;
+    return 0;
+}
+
+/** ----from here ---------------------------------------------------------- */
+
+struct UnionFind {
+    vector<int> par; // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
+
+    UnionFind(int N) : par(N) { //最初は全てが根であるとして初期化
+        for(int i = 0; i < N; i++) par[i] = i;
+    }
+
+    int root(int x) { // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
+        if (par[x] == x) return x;
+        return par[x] = root(par[x]);
+    }
+
+    void unite(int x, int y) { // xとyの木を併合
+        int rx = root(x); //xの根をrx
+        int ry = root(y); //yの根をry
+        if (rx == ry) return; //xとyの根が同じ(=同じ木にある)時はそのまま
+        par[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+    }
+
+    bool same(int x, int y) { // 2つのデータx, yが属する木が同じならtrueを返す
+        int rx = root(x);
+        int ry = root(y);
+        return rx == ry;
+    }
+};
+
+
+int main() {
+
+
+}
+
