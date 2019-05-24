1507f17560af13f1c90af9656a6278a6dbf06ed7
siwai <shoka.iwai@gmail.com>
2019-05-24 18:49:06 +0900

    Add ABC 010 B

---
+++ ABC/010/b.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+#define ld long double
+#define ll long long int
+#define ull unsigned long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector< vector<int> >
+#define vvl vector< vector<ll> >
+#define repd(i, a, b) for (int i=(a);i<(b);i++)
+#define rep(i, n) repd(i,0,n)
+#define ALL(v) v.begin(), v.end()
+#define INF 1e9
+
+using namespace std;
+
+/**
+ * calculate GCD(greatest common divisor)
+ * @param a
+ * @param b
+ * @return
+ */
+unsigned euclidean_gcd(unsigned a, unsigned b) {
+    if (a < b) return euclidean_gcd(b, a);
+    unsigned r;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+/**
+ * change minimum
+ * @tparam T
+ * @param a
+ * @param b
+ * @return bool
+ */
+template<class T>
+inline bool changeMinimum(T &a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * change maximum
+ * @tparam T
+ * @param a
+ * @param b
+ * @return bool
+ */
+template<class T>
+inline bool changeMaximum(T &a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * output answer
+ * @tparam T
+ * @param answer
+ * @return
+ */
+template<typename T>
+T output(T answer) {
+    cout << answer << endl;
+    return 0;
+}
+
+/** ----from here ---------------------------------------------------------- */
+
+int main() {
+
+    int N;
+    cin >> N;
+    vector<int> a(N);
+    int i;
+    rep(i, N) { cin >> a[i]; }
+
+    int ans = 0;
+    for (int i = 0; i < N; ++i) {
+        int n = a[i];
+
+        if (n % 6 == 1 || n % 6 == 3) {
+            continue;
+        }
+        if (n % 6 == 2 || n % 6 == 4) {
+            ans++;
+        }
+        if (n % 6 == 5) {
+            ans += 2;
+        }
+        if (n % 6 == 0) {
+            ans += 3;
+        }
+    }
+
+    cout << ans << endl;
+
+}
