94323863d89bf7f1340c4b8ca3ebaf62f0d1e43b
siwai <shoka.iwai@gmail.com>
2019-05-24 19:04:12 +0900

    Add ABC 010 C

---
+++ ABC/010/c.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+#define ld long double
+#define ll long long int
+#define ull unsigned long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector< vector<int> >
+#define vvl vector< vector<ll> >
+#define repd(i, a, b) for (int i=(a);i<(b);i++)
+#define rep(i, n) repd(i,0,n)
+#define ALL(v) v.begin(), v.end()
+#define INF 1e9
+
+using namespace std;
+
+/**
+ * calculate GCD(greatest common divisor)
+ * @param a
+ * @param b
+ * @return
+ */
+unsigned euclidean_gcd(unsigned a, unsigned b) {
+    if (a < b) return euclidean_gcd(b, a);
+    unsigned r;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+/**
+ * change minimum
+ * @tparam T
+ * @param a
+ * @param b
+ * @return bool
+ */
+template<class T>
+inline bool changeMinimum(T &a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * change maximum
+ * @tparam T
+ * @param a
+ * @param b
+ * @return bool
+ */
+template<class T>
+inline bool changeMaximum(T &a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * output answer
+ * @tparam T
+ * @param answer
+ * @return
+ */
+template<typename T>
+T output(T answer) {
+    cout << answer << endl;
+    return 0;
+}
+
+/** ----from here ---------------------------------------------------------- */
+
+int main() {
+
+
+    int xa, xb, ya, yb;
+    cin >> xa;
+    cin >> ya;
+    cin >> xb;
+    cin >> yb;
+
+    int T, V;
+    cin >> T;
+    cin >> V;
+    int N;
+    cin >> N;
+    vector<int> x(N);
+    vector<int> y(N);
+    int i;
+    rep(i, N) {
+        cin >> x[i];
+        cin >> y[i];
+    }
+    bool canMove = false;
+    int maxDistance = T * V;
+
+    for (int i = 0; i < N; ++i) {
+        double go = sqrt(pow((xa - x[i]), 2) + pow((ya - y[i]), 2));
+        double back = sqrt(pow((xb - x[i]), 2) + pow((yb - y[i]), 2));
+        double sum = go + back;
+        if (sum <= maxDistance) {
+            canMove = true;
+        }
+    }
+    string ans = "NO";
+    if (canMove) ans = "YES";
+
+    cout << ans << endl;
+
+}
