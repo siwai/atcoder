5c4a565ee1fde7ebba5e6531591604f8ddf4de07
siwai <shoka.iwai@gmail.com>
2019-07-05 16:15:52 +0900

    Add ABC 132 C

---
+++ ABC/132/c.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+#define ld long double
+#define ll long long int
+#define ull unsigned long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector< vector<int> >
+#define vvl vector< vector<ll> >
+#define repd(i, a, b) for (int i=(a);i<(b);i++)
+#define rep(i, n) repd(i,0,n)
+#define ALL(v) v.begin(), v.end()
+#define INF 1e9
+
+using namespace std;
+
+/**
+ * calculate GCD(greatest common divisor)
+ * @param a
+ * @param b
+ * @return
+ */
+unsigned euclidean_gcd(unsigned a, unsigned b) {
+    if (a < b) return euclidean_gcd(b, a);
+    unsigned r;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+/**
+ * change minimum
+ * @tparam T
+ * @param a
+ * @param b
+ * @return bool
+ */
+template<class T>
+inline bool changeMinimum(T &a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * change maximum
+ * @tparam T
+ * @param a
+ * @param b
+ * @return bool
+ */
+template<class T>
+inline bool changeMaximum(T &a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * output answer
+ * @tparam T
+ * @param answer
+ * @return
+ */
+template<typename T>
+T output(T answer) {
+    cout << answer << endl;
+    return 0;
+}
+
+/** ----from here ---------------------------------------------------------- */
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    vector<int> d(100010, 0);
+
+    for (int i = 0; i < N; ++i) {
+        int t;
+        cin >> t;
+        d[t]++;
+    }
+    int ans = 0;
+    if (N % 2 == 1) {
+        cout << ans << endl;
+        return 0;
+    }
+    int K = N / 2;
+    long long dp[100010];
+    for (int i = 0; i < 100008; ++i) {
+        dp[i + 1] = dp[i] + d[i];
+        if (dp[i + 1] == K) {
+            ans++;
+            cout << dp[i + 1] << endl;
+        }
+    }
+    cout << ans << endl;
+
+}
