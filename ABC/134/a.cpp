5ad04ae9cc9d64cd71f9c6ad87ee6224fab7b636
siwai <shoka.iwai@gmail.com>
2019-08-06 22:53:04 +0900

    Add ABC 133

---
+++ ABC/134/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+/** ----from here ---------------------------------------------------------- */
+
+int main() {
+
+
+    //int r ;cin >> r;
+
+//    cout<<3*pow(r,3)<<endl;
+
+    cout <<"-"<<endl;
+    return  0;
+}
